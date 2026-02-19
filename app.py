18ff0e3269d1d3250a77b27bedbae7d8369731f9
Punitpawar5 <155185502+Punitpawar5@users.noreply.github.com>
2026-02-19 11:02:10 +0530

    Updated application now working

--- app.py
+++ app.py
@@ -208,4 +208,4 @@ if __name__ == '__main__':
             print(f"✅ Database already contains {Property.query.count()} properties")
 
     # Start the Flask application
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host="0.0.0.0", port=5000, debug=True  )
